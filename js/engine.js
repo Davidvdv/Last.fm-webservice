631734186926f55e8f8b736da3eac446bfe39a67
Tiemen Waterreus <tiemen@tiemenwaterreus.com>
2012-04-27 13:50:51 +0200

    Get the individual track info

--- js/engine.js
+++ js/engine.js
@@ -1,7 +1,22 @@
 $(function(){
+	
+	
 	$.getJSON('http://ws.audioscrobbler.com/2.0/?method=user.getweeklytrackchart&user=jonne7&format=json&api_key=b25b959554ed76058ac220b7b2e0a026', function(data){
-		console.log(data);
-		alert( data.weeklytrackchart.track[0].artist);
-		console.log(data.weeklytrackchart.track[0].name);
+		
+		console.log(data.weeklytrackchart);
+		
+		for(i in data.weeklytrackchart.track)
+		{	tr = data.weeklytrackchart.track[i].name;
+			ar = data.weeklytrackchart.track[i].artist["#text"];
+			
+			$.getJSON('http://ws.audioscrobbler.com/2.0/',{method:'track.getInfo', api_key: 'b25b959554ed76058ac220b7b2e0a026', artist:ar, track:tr}, function(d){
+				console.log('something');
+				console.log(d);
+			});
+		}
+		
+		
+
+		
 	});
 });
