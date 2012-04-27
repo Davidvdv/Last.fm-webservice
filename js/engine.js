f1afa2351310b71b0a773425c128df169c7ff985
Tiemen Waterreus <tiemen@tiemenwaterreus.com>
2012-04-27 14:50:43 +0200

    Fetch duration per track

--- js/engine.js
+++ js/engine.js
@@ -1,22 +1,31 @@
 $(function(){
 	
+	var username = 'tmw014';
+	var graphData = [['Track', 'Duration']];
 	
-	$.getJSON('http://ws.audioscrobbler.com/2.0/?method=user.getweeklytrackchart&user=jonne7&format=json&api_key=b25b959554ed76058ac220b7b2e0a026', function(data){
+	$.getJSON('http://ws.audioscrobbler.com/2.0/?method=user.getweeklytrackchart&user='+username+'&format=json&api_key=b25b959554ed76058ac220b7b2e0a026', function(data){
 		
 		console.log(data.weeklytrackchart);
 		
 		for(i in data.weeklytrackchart.track)
-		{	tr = data.weeklytrackchart.track[i].name;
+		{	
+			tr = data.weeklytrackchart.track[i].name;
+			pl = data.weeklytrackchart.track[i].playcount;
 			ar = data.weeklytrackchart.track[i].artist["#text"];
 			
-			$.getJSON('http://ws.audioscrobbler.com/2.0/',{method:'track.getInfo', api_key: 'b25b959554ed76058ac220b7b2e0a026', artist:ar, track:tr}, function(d){
-				console.log('something');
-				console.log(d);
-			});
+			process(tr, pl, ar);
 		}
 		
-		
-
-		
 	});
+	
+	function process(tr, pl, ar){
+		req = $.getJSON('http://ws.audioscrobbler.com/2.0/',{method:'track.getInfo', user: 'jonne7', format: 'json', api_key: 'b25b959554ed76058ac220b7b2e0a026', artist:ar, track:tr});
+
+		req.complete(function(d){
+			duration = $.parseJSON(d.responseText).track.duration * pl;
+			console.log(tr + ' - ' +duration);
+			
+			graphData.push([tr, duration]);
+		});
+	}
 });
