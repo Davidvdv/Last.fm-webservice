66d52d5ea69a5af082dc3f586b723d87531836b3
David van de Vondervoort <d.vandevondervoort@gmail.com>
2012-05-07 13:29:04 +0200

    Form submit by jQuery .submit() and some CSS changes

--- js/main.js
+++ js/main.js
@@ -4,19 +4,10 @@ $(document).ready(function(){
 	// Set focus to the input field by default
 	$("#username").focus();
 	
-	// When the user clicks the button
-	$('#go').click(function(){
-		// Initialize and runn the app with the given username
+	// When the user submits the form
+	$('#form').submit(function(){
 		window.app = new Engine($('#username').val());
-	});
-	
-	// When the user presses a button inside the username-field
-	$('#username').keyup(function(e){
-		// and it appears to be the return key
-		if(e.keyCode == 13){
-			// Run the application with the given username
-			window.app = new Engine($('#username').val());
-		}
+		return false;
 	});
 	
 });
