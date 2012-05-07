dfac4388991a0785fbe1c1100ead8caef7ed9c89
David van de Vondervoort <d.vandevondervoort@gmail.com>
2012-05-07 14:10:18 +0200

    Remove log statements

--- js/chart.js
+++ js/chart.js
@@ -3,10 +3,9 @@ google.load("visualization", "1", {packages:["corechart"]});
 function drawChart(data, username){
 	
 	var data = google.visualization.arrayToDataTable(data);	
-	console.log(username);
 	var options = {
 		title: username,
-		hAxis: {title: 'Tracks', titleTextStyle: {color: 'black'}}
+		hAxis: {title: 'Tracks', titleTextStyle: {color: 'black'}},
 	};
 
 	var chart = new google.visualization.ColumnChart(document.getElementById('chart'));
